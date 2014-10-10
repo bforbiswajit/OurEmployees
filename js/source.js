613ab880d0d5f5314a0236dce72c3f310099787d
Danish Nadaf <danishnadaf@gmail.com>
2014-10-10 13:35:45 +0530

    Update source.js

--- js/source.js
+++ js/source.js
@@ -1,1 +1,34 @@
 // Code for Javascript, Ajax, Jquery Here
+
+$(document).ready(function(){
+    
+
+                                        /*Jquery for Admin Panel tabs*/
+    $('#tab-links a').click(function(e){
+         e.preventDefault();
+        if($(this).attr('class') == 'current')
+        {
+            $(this).parent().children().removeClass('current');
+            $('#tabs '+ $(this).attr('href')).parent().children().hide();
+        }else{
+            $(this).addClass('current').parent().siblings().children().removeClass('current');
+            $('#tabs '+ $(this).attr('href')).slideDown().siblings().slideUp();
+        }
+    });
+                                    /*Jquery for Admin Panel tabs Ends*/
+    
+                                    /*Jquery for My Profile Pop Up Start*/
+    $('#bottonMyProfile').click(function(){
+       $('#popUpContainer').fadeIn(300);
+       $('#divMyProfile').fadeIn(500);
+    });
+    
+    $('#closeContainer').click(function(){
+        $('#popUpContainer').fadeOut(500);
+        $('#divMyProfile').fadeOut(500);
+    });
+                                    /*Jquery for My Profile Pop Up Ends*/
+    
+    
+    
+});
